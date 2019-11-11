5f7aeddcd9b3d34c9b50c25b1dc27b4481e46dd1
Hsu <littleorange0812@gmail.com>
2019-11-11 11:29:38 +0800

    Update KPermission.swift
    fix bug

--- Sources/WKFrameworkLibrary/KPermission.swift
+++ Sources/WKFrameworkLibrary/KPermission.swift
@@ -13,7 +13,7 @@ import Photos
 public struct KPermission {
     
     /// 取的麥克風權權限狀態
-    func getMicrophonePermission () -> AVAudioSession.RecordPermission {
+    public func getMicrophonePermission () -> AVAudioSession.RecordPermission {
         return AVAudioSession.sharedInstance().recordPermission
     }
     
@@ -23,7 +23,7 @@ public struct KPermission {
     ///.restricted：使用者不允許訪問媒體捕獲設備。
     ///.denied：使用者明確的拒絕媒體捕獲的權限。
     ///.authorized ：使用者明確的允許媒體捕獲權限，或者對於相關媒體類型不需要明確的使用者權限。
-    func getCameraPermission(AVMediaType type:AVMediaType) -> AVAuthorizationStatus{
+    public func getCameraPermission(AVMediaType type:AVMediaType) -> AVAuthorizationStatus{
         return AVCaptureDevice.authorizationStatus(for: type)
     }
     
@@ -33,7 +33,7 @@ public struct KPermission {
     ///.restricted：使用者不允許訪問媒體捕獲設備。
     ///.denied：使用者明確的拒絕媒體捕獲的權限。
     ///.authorized ：使用者明確的允許媒體捕獲權限，或者對於相關媒體類型不需要明確的使用者權限。
-    func getPHPhotoPermission() -> PHAuthorizationStatus{
+    public func getPHPhotoPermission() -> PHAuthorizationStatus{
         return PHPhotoLibrary.authorizationStatus()
     }
     
